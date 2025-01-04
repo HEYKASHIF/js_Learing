12922c264047fb3a9abb3529e67c43c723c63ccd
HEYKASHIF <kashifmohd269@gmail.com>
2025-01-04 16:40:58 +0530

    second commit

---
+++ Day02.js
@@ -0,0 +1,18 @@
+//Create an array that stores the temperatures recorded over 7 days. 
+//Use indexing to access the temperature for the 4th day and update it
+//to a new value by adding 2 degrees. After that, log the updated temperatures 
+//for all 7 days to the console.
+
+
+// let arrTemp = [10, 11, 9, 8, 13, 10, 5];
+
+// arrTemp[3] = arrTemp[3]+2;
+
+// console.log(arrTemp);
+
+//Problem Statement: Write a function called greetUser that prompts the user to enter their name using the prompt() function.
+//If the user enters a name, greet them with the message: "Hello, !". If no name is provided (i.e., the user presses "Cancel" 
+// or enters an empty string), the function should use a default name "Guest" and greet them with: "Hello, Guest!".
+//equirements:
+//Use a function expression. Ensure the function handles cases where the user cancels or leaves the input empty.
+
