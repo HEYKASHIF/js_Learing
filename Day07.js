babe2568fa8d105a9bda4955cbbb6dd26beb6e78
HEYKASHIF <kashifmohd269@gmail.com>
2025-01-12 23:11:45 +0530

    _assignment-deadline

--- Day07.js
+++ Day07.js
@@ -26,9 +26,8 @@
 //   row = "";
 // }
 
-
-// You are given a number stored in a variable with the name N 
-// You have to print the pattern as shown below according to the value stored in N For example, 
+// You are given a number stored in a variable with the name N
+// You have to print the pattern as shown below according to the value stored in N For example,
 // consider the value stored in N = 5, then the required pattern will be.
 
 // * * * * *
@@ -40,4 +39,53 @@
 // *
 
 // * * * * *
+
+// let N = 5;
+// let row = "";
+// for (let i = 0; i < N; i++) {
+//   if (i === 0 || i === N - 1) {
+//     for (let j = 0; j < N; j++) {
+//       row = row + " " + "*";
+//     }
+//     console.log(row);
+//     row = "";
+//   } else {
+//     console.log("*");
+//   }
+// }
+
+// Problem Statement : Given a non-negative integerk, decide whether
+// there're two integers a and b such that a^2+ b^2= k.
+
+// Hint : In the first sample test case, the value stored is = 25
+
+// for the values a = 1, b = 2 , the sum of square = a*a + b*b = 3*3 + 4*4 = 25 ,which is equal to 25
+
+// Thus the output is true.
+
+// In the second sample test case, the value stored is = 7
+
+// there is no value of a and b exists, which will be equal to c = 7
+
+// Thus the output is false.
+
+// let integer = 125;
+// let str = integer.toString();
+// console.log(str);
+
+// if (str.length > 1) {
+//   let sumOfSquare = 0;
+
+//   for (let i = 0; i < str.length; i++) {
+//     sumOfSquare = str[i] ** 2 + sumOfSquare;
+//   }
+//   if (sumOfSquare == integer) {
+//     console.log("true");
+//   } else {
+//     console.log("false");
+//   }
+// } else {
+//   console.log("false");
+// }
+
 
