967ab2031b083993f55d0a60ea44782819db2d1b
HEYKASHIF <kashifmohd269@gmail.com>
2025-01-12 22:44:18 +0530

    assignment-deadline

---
+++ Day07.js
@@ -0,0 +1,43 @@
+// You are given a number stored in a variable with the name N You have to print
+// all the numbers in the range from 1 to N*N, such that there are exactly N numbers on each line
+
+// For example, if the value stored inN = 3, then all the numbers in the range, from [1,9]
+// need to be printed, such that there are 3 numbers on each line. Therefore, the required output is
+
+// 1 2 3
+
+// 4 5 6
+
+// 7 8 9
+
+// let arr = [
+//   [1, 2, 3, 4],
+//   [1, 2, 1, 2],
+//   [1, 3, 1, 3],
+//   [1, 4, 1, 4],
+// ];
+// let N = 4;
+// let row = "";
+// for (let i = 0; i < N; i++) {
+//   for (let j = 0; j < N; j++) {
+//     row = row+' '+arr[i][j];
+//   }
+//   console.log(row);
+//   row = "";
+// }
+
+
+// You are given a number stored in a variable with the name N 
+// You have to print the pattern as shown below according to the value stored in N For example, 
+// consider the value stored in N = 5, then the required pattern will be.
+
+// * * * * *
+
+// *
+
+// *
+
+// *
+
+// * * * * *
+
--- index.js
+++ index.js
@@ -34,3 +34,4 @@
 // }
 
 // console.log(calculator.add())
+
