c5b95566f41430d0f53de64c48d774aff061b19e
HEYKASHIF <kashifmohd269@gmail.com>
2025-01-18 00:09:07 +0530

    swap_array

--- Day08.js
+++ Day08.js
@@ -7,9 +7,27 @@
 // let book = { title: "The Hobbit", author: "J.R.R. Tolkien", year: 1937 };
 
 
-let book = { title: "Do epic shit", author: "Ankur warikoo", year: 2021 };
+// let book = { title: "Do epic shit", author: "Ankur warikoo", year: 2021 };
 
-for(let item in book)
-{
-    console.log(`${item} : ${book[item]}`);
-}
+// for(let item in book)
+// {
+//     console.log(`${item} : ${book[item]}`);
+// }
+
+let arr = ['A','B','C','D','E','F'];
+let N = arr.length;
+
+let newarr = [];
+for(let i=0; i<N; i++)
+    { 
+      
+      if(i%2==0)
+      {
+        // let temp =0;
+        newarr[i] = arr[i+1];
+        newarr[i+1] = arr[i];
+      }
+    }
+    
+    console.log(newarr);
+      //write code here
--- index.js
+++ index.js
@@ -35,3 +35,4 @@
 
 // console.log(calculator.add())
 
+console.log(36%36);
