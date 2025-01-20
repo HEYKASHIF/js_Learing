8c7a6891e0c1395feb13fb0e03237323227665e9
HEYKASHIF <kashifmohd269@gmail.com>
2025-01-21 00:09:46 +0530

    _DSA_contest

---
+++ Day10.js
@@ -0,0 +1,24 @@
+// function niceArray(N, array, K) {
+  
+//     let count = 0;
+//     for(let i=N-1; i>=0; i--)
+//     {
+//       if(array[i]%2==0)
+//       {
+//         count++;
+//       }
+//     }
+    
+//     if(count>K)
+//     {
+//       console.log('Nice Array');
+//     }
+//     else
+//     {
+//       console.log('Bad Array');
+//     }
+//     //write code here
+    
+    
+//   }
+  
