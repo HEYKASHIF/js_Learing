17a0097dc18172a0d64cf16356482d034553a8c4
HEYKASHIF <kashifmohd269@gmail.com>
2025-01-24 23:52:00 +0530

    pseudocode

---
+++ Day11.js
@@ -0,0 +1,25 @@
+// Write the Pseudocode for the below:
+
+// You are given three numbers stored in three variables, with the names num1, num2, and num3.
+
+// Print the result of the following operations:
+
+// num1 > num2 && num1 > num3-> this operation prints true if num1 is greater than both num2 and num3, otherwise it prints false.
+// num2 > num1 && num2 > num3 -> this operation prints true if num2 is greater than both num1 and num3, otherwise it prints false.
+// num3 > num1 && num3 > num2 -> this operation prints true if num3 is greater than both num1 and num2, otherwise it prints false.
+
+// num1 = 10;
+// num2 = 20;
+// num3 = 30;
+
+// if (num1 > num2 && num1 > num3) print("true");
+// else print("false");
+
+// if (num2 > num1 && num2 > num3) print("true");
+// else print("false");
+
+// if (num3 > num1 && num3 > num2) print("true");
+// else print("false");
+
+
+
