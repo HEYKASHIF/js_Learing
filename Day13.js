44b5e4b18035adc7a7e75b8e0223579ccbe220a5
HEYKASHIF <kashifmohd269@gmail.com>
2025-01-28 21:48:42 +0530

    PSC_conditinalStatements_loops

---
+++ Day13.js
@@ -0,0 +1,61 @@
+//print factors!
+// function printfactors(n)
+// {
+//     for(let i=1; i<=n; i++)
+//     {
+//         if(n%i==0)
+//         {
+//             console.log(i);
+//         }
+//     }
+
+// }
+
+// printfactors(12);
+
+
+
+//factors count!
+// function factors_counts(n)
+// {
+//     let count =0;
+//     for(let i=1; i<=n; i++)
+//     {
+//         if(n%i==0)
+//         {
+//             count++;
+//         }
+//     }
+//     console.log(count);
+
+// }
+
+// factors_counts(12);
+
+
+
+
+//check Prime!
+// function isPrime(n)
+// {
+//     let count = 0;
+//     for(let i=1; i<=n; i++)
+//     {
+//         if(n%i==0)
+//         {
+//             count++;
+//         }
+//     }
+
+//     if(count===2)
+//     {
+//         console.log('Prime');
+//     }
+//     else
+//     {
+//         console.log('Not a Prime');
+//     }
+// }
+
+// isPrime(5);
+
