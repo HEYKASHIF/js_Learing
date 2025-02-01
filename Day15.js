c2eb4fffae5d40fade559b84fc47c2305b65b384
HEYKASHIF <kashifmohd269@gmail.com>
2025-02-01 22:37:39 +0530

    ASCII

---
+++ Day15.js
@@ -0,0 +1,11 @@
+//ASCII Problems!
+
+let S = 'aba';
+let sum = 0;
+for(let i=0; i<S.length; i++)
+{
+    sum = sum + (S.charCodeAt(i)-96);
+}
+console.log('Z'.charCodeAt(0));
+
+String.fromCharCode(90 - 1);
