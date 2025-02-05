342ee308b89c42926b0b28d7395aaec70596f2ce
HEYKASHIF <kashifmohd269@gmail.com>
2025-02-05 19:53:46 +0530

    problem

--- Day15.js
+++ Day15.js
@@ -12,35 +12,52 @@
 
 
 //Good-Array or Bad-Array
-
-let arr = [1,2,3,4,5,6,7,8,9];
-let N = arr.length;
-let K = Math.floor(N/5);
-function niceArray(N, array, K) {
+// let arr = [1,2,3,4,5,6,7,8,9];
+// let N = arr.length;
+// let K = Math.floor(N/5);
+// function niceArray(N, array, K) {
   
-    let evenCount = 0;
+//     let evenCount = 0;
     
-    for(let i=0; i<N; i++)
-    {
-      if(array[i]%2==0)
-      {
-        evenCount++;
-      }
-    }
-    // console.log(K)
-    // console.log(evenCount)
+//     for(let i=0; i<N; i++)
+//     {
+//       if(array[i]%2==0)
+//       {
+//         evenCount++;
+//       }
+//     }
+//     // console.log(K)
+//     // console.log(evenCount)
     
-    if(K<evenCount)
+//     if(K<evenCount)
+//     {
+//       console.log('Nice Array');
+//     }
+//     else
+//     {
+//       console.log('Bad Array');
+//     }
+//     //write code here
+    
+    
+//   }
+
+//   niceArray(N, arr, K);
+
+
+
+let S = 'a#bc';
+
+let empty = '';
+for(let i=0; i<S.length; i++)
+{
+    if(S[i+1]=='#' || S[i]=='#')
     {
-      console.log('Nice Array');
+        continue;
     }
     else
     {
-      console.log('Bad Array');
+        empty+=S[i];
     }
-    //write code here
-    
-    
-  }
-
-  niceArray(N, arr, K);
+}
+console.log(empty);
