085e0467f6bfea219113e4ccf1edd7b4bb7d24c9
HEYKASHIF <kashifmohd269@gmail.com>
2025-03-06 19:10:33 +0530

    Intro to LinkedList

--- Day23.js
+++ Day23.js
@@ -36,7 +36,176 @@
 //     start++;
 //   }
 //   return temp.data;
-  
-    
 // };
+
+
+
+
+
+//Inserting a node at head!
+// const LinkedListNode = class {
+//     constructor(nodeData) {
+//         this.data = nodeData;
+//         this.next = null;
+//     }
+// };
+
+// // Complete the function below
+
+// function insertNodeAtHead(head, data) {
+  
+//   let tempNode = new LinkedListNode(data);
+//   tempNode.next = head;
+//   head = tempNode;
+//   return head;
+// }
+
+
+
+
+
+
+//Deleting the MIddle Node!
+// const LinkedListNode = class {
+//     constructor(nodeData) {
+//         this.data = nodeData;
+//         this.next = null;
+//     }
+// };
+
+// var deleteMiddle = function(head, n) {
+  
+//   if(head==null)
+//   {
+//     return null;
+//   }
+//   let temp = head;
+//   let middle = Math.floor(n/2);
+//   let i=1;
+//   while(i<middle)
+//   {
+//     temp=temp.next;
+//     i++;
+//   }
+//   temp.next=temp.next.next;
+//   return head;
+// };
+
+
+
+
+
+//Inserting a Node at a Tail!
+// const LinkedListNode = class {
+//     constructor(nodeData) {
+//         this.data = nodeData;
+//         this.next = null;
+//     }
+// };
+
+// // Complete the function below
+
+// function insertNodeAtTail(head, data) {
+    
+//     let tempNode = new LinkedListNode(data);
+//     let current = head;
+//     if(current==null)
+//     {
+//       return tempNode;
+//     }
+//     while(current.next != null)
+//     {
+//       current = current.next;
+//     }
+//     current.next = tempNode;
+//     return head;
+// }
+
+
+
+
+
+
+//Inserting at a specific position!
+// const LinkedListNode = class {
+//     constructor(nodeData) {
+//         this.data = nodeData;
+//         this.next = null;
+//     }
+// };
+
+// // Complete the function below
+
+// function insertNodeAtPosition(head, data, position) {
+  
+//   let newNode = new LinkedListNode(data);
+  
+//   if(position == 0)
+//   {
+//     newNode.next = head;
+//     return newNode;
+//   }
+  
+//   let firstNode = head;
+//   let i=1;
+//   while(i<position)
+//   {
+//     if(firstNode==null || firstNode.next==null)
+//     {
+//       return head;
+//     }
+//     firstNode = firstNode.next;
+//     i++;
+//   }
+  
+//   let secondNode = firstNode.next;
+//   firstNode.next = newNode;
+//   newNode.next = secondNode;
+  
+//   return head;
+// }
+
+
+
+
+
+
+//Deleting a Node at a Specific Position!
+// const LinkedListNode = class {
+//     constructor(nodeData) {
+//         this.data = nodeData;
+//         this.next = null;
+//     }
+// };
+
+// // Complete the function below
+
+// function deleteNode(head, position) {
+//   if(head==null)
+//   {
+//     return null;
+//   }
+//   let temp = head;
+//   let i=1;
+//   while(i<position)
+//   {
+//     if(temp.next!=null)
+//     {
+//       temp=temp.next;
+//       i++;
+//     }
+//     else
+//     {
+//       return null;
+//     }
+//   }
+//   temp.next=temp.next.next;
+//   return head;
+// }
+
+
+
+
+
+
 
