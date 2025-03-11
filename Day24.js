75f24952b445b5e70fdd0f940f23d8bf793ac8ae
HEYKASHIF <kashifmohd269@gmail.com>
2025-03-11 11:30:17 +0530

    bitwise_assignments

--- Day24.js
+++ Day24.js
@@ -1,2 +1,18 @@
 //BitWise Operator!
-console.log(28<<1);
+// console.log(28<<1);
+
+
+
+
+
+//All Twice Except one!
+function allTwiceExceptOne(n, arr){
+    let a=arr[0];
+    for(let i=1; i<n*2; i++)
+    {
+      a = a^arr[i];
+      // console.log(a);
+    }
+    console.log(a);
+    // write your code here
+  }
