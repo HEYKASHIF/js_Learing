988a083fc729772173f0ee06261e44c48b3a9fab
HEYKASHIF <kashifmohd269@gmail.com>
2025-03-11 12:34:15 +0530

    binary conversion

--- Day24.js
+++ Day24.js
@@ -6,13 +6,52 @@
 
 
 //All Twice Except one!
-function allTwiceExceptOne(n, arr){
-    let a=arr[0];
-    for(let i=1; i<n*2; i++)
-    {
-      a = a^arr[i];
-      // console.log(a);
-    }
-    console.log(a);
-    // write your code here
-  }
+// function allTwiceExceptOne(n, arr){
+//     let a=arr[0];
+//     for(let i=1; i<n*2; i++)
+//     {
+//       a = a^arr[i];
+//       // console.log(a);
+//     }
+//     console.log(a);
+//     // write your code here
+//   }
+
+
+
+
+
+//Power of 2!
+// function powerOfTwo(n)
+// {
+  
+//   if((n&n-1)==0)
+//   {
+//     console.log('True');
+//   }
+//   else
+//   {
+//     console.log('False');
+//   }
+//   // console.log(n);
+// }
+
+
+
+
+//Convert To Binary!
+// function convertToBinary(N)
+// {
+//     console.log(N);
+//   let binary = '';
+//   while(N!=1)
+//   {
+//     binary=N%2+binary;
+//     N=parseInt(N/2);
+//   }
+//   binary=N+binary;
+//   console.log(Number(binary));
+//   return Number(binary);
+// }
+
+// convertToBinary(4);
