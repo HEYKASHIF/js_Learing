97482eb3e7833f91e625741b2c0041e5544fd347
HEYKASHIF <kashifmohd269@gmail.com>
2025-03-27 22:06:06 +0530

    binary search

--- Day26.js
+++ Day26.js
@@ -122,3 +122,44 @@
 // }
 
 
+
+
+
+
+//Lower Bound in LOG(n) Time!
+// function lowerBound(k,n,arr)
+// {
+//   let low = 0;
+//   let high = n-1;
+//   let res = -1;
+//   while(low<=high)
+//   {
+//     let mid = low+Math.floor((high-low)/2);
+//     if(k==arr[mid])
+//     {
+//       res=mid;
+//       high=mid-1;
+//     }
+//     else if(k<arr[mid])
+//     {
+//       high=mid-1;
+//     }
+//     else
+//     {
+//       low=mid+1;
+//     }
+//   }
+//   return res;
+// }
+// function runProgram(input)
+//{
+//   input = input.split('\n');
+//   let line1 = input[0].split(' ').map(Number);
+//   let n = line1[0];
+//   let k = line1[1];
+//   let arr = input[1].split(' ').map(Number);
+//   let res = lowerBound(k,n,arr);
+//   console.log(res);
+// }
+
+
