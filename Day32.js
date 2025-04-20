1f64fcf6f9950353e00be9b15270a72981992d31
HEYKASHIF <kashifmohd269@gmail.com>
2025-04-21 00:52:53 +0530

    Higher Order Function

--- Day32.js
+++ Day32.js
@@ -127,6 +127,8 @@
 
 
 
+
+
 //Problem Statement:
 // You are given an array of student objects. Each student object contains a name and marks. 
 // Write a function processStudents that performs the following steps:
@@ -157,47 +159,72 @@
 //OUTPUT-
 // ["Hannah", "Charlie", "Mia", "Grace", "Leo", "Bob", "Olivia", "Jack", "Emma", "Nathan", "Kate", "Frank"]
 
+// let input = 
+// [
+//     { "name": "Alice", "marks": 58 },
+//     { "name": "Bob", "marks": 85 },
+//     { "name": "Charlie", "marks": 92 },
+//     { "name": "David", "marks": 45 },
+//     { "name": "Emma", "marks": 76 },
+//     { "name": "Frank", "marks": 63 },
+//     { "name": "Grace", "marks": 89 },
+//     { "name": "Hannah", "marks": 95 },
+//     { "name": "Ian", "marks": 54 },
+//     { "name": "Jack", "marks": 79 },
+//     { "name": "Kate", "marks": 67 },
+//     { "name": "Leo", "marks": 88 },
+//     { "name": "Mia", "marks": 91 },
+//     { "name": "Nathan", "marks": 72 },
+//     { "name": "Olivia", "marks": 82 }
+// ]
 
-
-let input = 
-[
-    { "name": "Alice", "marks": 58 },
-    { "name": "Bob", "marks": 85 },
-    { "name": "Charlie", "marks": 92 },
-    { "name": "David", "marks": 45 },
-    { "name": "Emma", "marks": 76 },
-    { "name": "Frank", "marks": 63 },
-    { "name": "Grace", "marks": 89 },
-    { "name": "Hannah", "marks": 95 },
-    { "name": "Ian", "marks": 54 },
-    { "name": "Jack", "marks": 79 },
-    { "name": "Kate", "marks": 67 },
-    { "name": "Leo", "marks": 88 },
-    { "name": "Mia", "marks": 91 },
-    { "name": "Nathan", "marks": 72 },
-    { "name": "Olivia", "marks": 82 }
-]
-
-let filtered = input.filter(function (obj)
-{
-    return (obj.marks>60) ;
+// let filtered = input.filter(function (obj)
+// {
+//     return (obj.marks>60) ;
     
-})
+// })
 
-console.log(filtered);
+// console.log(filtered);
 
-filtered.sort(function (a, b)
-{
-   b= b.marks;
-   a= a.marks;
-   return b-a;
-});
+// filtered.sort(function (a, b)
+// {
+//    b= b.marks;
+//    a= a.marks;
+//    return b-a;
+// });
 
-console.log(filtered);
+// console.log(filtered);
 
-let mapped = filtered.map(function(obj)
-{
-    return obj.name;
-})
+// let mapped = filtered.map(function(obj)
+// {
+//     return obj.name;
+// })
 
-console.log(mapped);
+// console.log(mapped);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let input = ["electronics", "clothing", "electronics", "toys", "clothing", "toys", "toys"];
+
+// let output = input.reduce(function (acc, curr)
+// {
+//     if (acc[curr]) {
+//         acc[curr] = acc[curr] + 1;
+//     } else {
+//         acc[curr] = 1;
+//     }
+//     return acc;
+// }, {});
+
+// console.log(output);
